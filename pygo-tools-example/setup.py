26d88eb13da218260a683a57f567e5d4ad9d39f3
Rajan Khullar <rkhullar03@gmail.com>
2024-02-02 11:23:32 -0500

    rename pygo-tools-example (#6)

--- pygo-tools-example/setup.py
+++ pygo-tools-example/setup.py
@@ -2,7 +2,7 @@ from pygo_tools import setup
 from setuptools import find_packages
 
 setup(
-    name='example',
+    name='pygo-tools-example',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True
