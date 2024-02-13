4308a800cd3089bd2d08d7b4532d70a0ff68550e
Rajan Khullar <rkhullar03@gmail.com>
2024-02-13 18:33:15 -0500

    save

---
+++ fastapi-mongodb/fastapi_mongodb/router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter as DefaultRouter
+
+
+class APIRouter(DefaultRouter):
+    """
+    custom base api router for allowing route handlers to be decorated
+    could be useful for roles and permissions
+    TODO: confirm if still needed and add example or test
+    """
+
+    def api_route(self, *args, **kwargs):
+        parent_decorator = super().api_route(*args, **kwargs)
+
+        def decorator(fn):
+            parent_decorator(fn)
+            fn.api_route = self.routes[-1]
+            assert fn == fn.api_route.endpoint
+            return fn
+
+        return decorator
---
+++ fastapi-mongodb/fastapi_mongodb/util/__init__.py
@@ -0,0 +1,1 @@
+from .mongo import is_valid_object_id, build_atlas_client, build_filter_params, start_atlas_transaction
