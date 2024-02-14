92f52890d55f56e24d498949c9cf365ac3bd6a40
Rajan Khullar <rkhullar03@gmail.com>
2024-02-14 16:04:51 -0500

    save

--- generic-fastapi/api/factory.py
+++ generic-fastapi/api/factory.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from .config import Settings
 # from .model.adapter import MongoAdapterCache
 from .router import router as api_router
-from fastapi_tbd.metrics import add_process_time_header
+from fastapi_tools.metrics import add_process_time_header
 
 
 def create_app(settings: Settings, test: bool = False) -> FastAPI:
@@ -19,7 +19,7 @@ def create_app(settings: Settings, test: bool = False) -> FastAPI:
     # mongo_client = build_atlas_client(atlas_host=settings.atlas_host, local_mode=settings.local_mode)
     # app.extra['atlas'] = MongoAdapterCache(mongo_client=mongo_client)
 
-    app.add_middleware(add_process_time_header)
-    #@app.middleware('http')(add_process_time_header)
+    #app.add_middleware(add_process_time_header)
+    app.middleware('http')(add_process_time_header)
 
     return app
