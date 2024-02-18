a652c3fc99560ca5f615f739852717424e0aeb21
Rajan Khullar <rkhullar03@gmail.com>
2024-02-18 18:19:30 -0500

    save

--- generic-fastapi/api/factory.py
+++ generic-fastapi/api/factory.py
@@ -4,6 +4,7 @@ from fastapi_tools.mongo import MongoAdapterCache, build_atlas_client
 
 from .config import Settings
 from .router import router as api_router
+from .model.user import User
 
 
 def create_app(settings: Settings, test: bool = False) -> FastAPI:
@@ -18,7 +19,8 @@ def create_app(settings: Settings, test: bool = False) -> FastAPI:
     )
     app.include_router(api_router)  # prefix='/api'
     mongo_client = build_atlas_client(atlas_host=settings.atlas_host, local_mode=settings.local_mode)
-    app.extra['atlas'] = MongoAdapterCache(mongo_client=mongo_client)
+    app.extra['atlas'] = mongo_adapter_cache = MongoAdapterCache(mongo_client=mongo_client, default_database='test')
+    app.extra['users'] = mongo_adapter_cache.adapter(collection='user', model_type=User)
     # app.add_middleware(add_process_time_header)
     app.middleware('http')(add_process_time_header)
 
--- generic-fastapi/api/schema/user.py
+++ generic-fastapi/api/schema/user.py
@@ -1,5 +1,4 @@
 from typing import Self
-
 from fastapi_tools.auth import AbstractUser
 from fastapi_tools.auth.auth0 import Auth0IdentityToken
 
@@ -11,8 +10,16 @@ class User(AbstractUser):
 
     @classmethod
     async def load(cls, auth_data: dict, identity_token: Auth0IdentityToken, context: dict) -> Self:
-        return cls.model_validate({
-            'name': identity_token.name,
-            'email': identity_token.email,
-            'auth0_id': auth_data['sub']
-        })
+        # TODO: rename to from_auth?
+        users = context['user']
+        if doc := users.find_one({'email': identity_token.email}):
+            # NOTE: update user attributes?
+            return cls.model_validate(doc.model_dump())
+        else:
+            to_insert = {
+                'name': identity_token.name,
+                'email': identity_token.email,
+                'auth0_id': auth_data['sub']
+            }
+            response = users.collection.insert_one(to_insert)
+            return cls(id=response.inserted_id, **to_insert)
