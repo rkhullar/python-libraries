8b76f0660cebcd5a9edfca0d525b1428381e7ed8
Rajan Khullar <rkhullar03@gmail.com>
2024-02-19 17:47:47 -0500

    save

--- generic-fastapi/api/routes/admin/router.py
+++
@@ -1,6 +0,0 @@
-from fastapi_tools import APIRouter
-
-from . import generic_shared_data
-
-router = APIRouter()
-router.include_router(generic_shared_data.router, prefix='/shared-data', tags=['shared-data'])
