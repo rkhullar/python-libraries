326914896f810aedf06a3e4ecff5e827f5f6518a
Rajan Khullar <rkhullar03@gmail.com>
2024-02-21 11:48:16 -0500

    save

--- examples/pygo-tools/_hello/docker/wheel/docker-build.sh
+++ examples/pygo-tools/_hello/docker/wheel/docker-build.sh
@@ -5,8 +5,17 @@ here=$(dirname "$PWD"/"$0")
 here=$(cd "$here" || exit; pwd)
 target_path=$(cd "${here}/../.." || exit; pwd)
 
+rm -rf "$here/local"; mkdir "$here/local"
+docker_context="$here/local/docker-context.tar"
+
 cd "$target_path" || exit
 make clean
-# tar --exclude='local' --exclude='venv' -cvh ./* | docker build -t pygo-hello-build -
-tar --exclude='local' --exclude='venv' -cvf "$here/local/docker-context.tar" ./*
+tar --exclude='local' --exclude='venv' -cvf "$docker_context" ./*
+
+cd "$here" ||
+tar --append --file "$docker_context" Dockerfile
+docker build -t pygo-hello-build - < "$docker_context"
 # docker compose up
+
+## other
+# tar --exclude='local' --exclude='venv' -cvh ./* | docker build -t pygo-hello-build -
