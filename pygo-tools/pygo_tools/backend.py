927877fc8ce4fdfbedbcb32b7b057849a54ea5ce
Rajan Khullar <rkhullar03@gmail.com>
2024-02-21 23:37:24 -0500

    save

--- pygo-tools/pygo_tools/backend.py
+++ pygo-tools/pygo_tools/backend.py
@@ -1,9 +1,20 @@
-from setuptools.build_meta import build_wheel as _build_wheel
+from setuptools import build_meta as _build_meta
+
 from .setup import precompile, patch_wheel_darwin, find_wheel
 
 
-def build_wheel(*args, **kwargs):
+def get_requires_for_build_wheel(config_settings=None):
+    result = _build_meta.get_requires_for_build_wheel(config_settings)
+    if 'cffi' not in result:
+        result.append('cffi')
+    return result
+
+
+def build_wheel(*args, config_settings=None, **kwargs):
     print('custom build_wheel')
     print(args)
     print(kwargs)
-    return _build_wheel(*args, **kwargs)
+    print("*"*100)
+    print(config_settings)
+    print("*"*100)
+    return _build_meta.build_wheel(*args, **kwargs)
