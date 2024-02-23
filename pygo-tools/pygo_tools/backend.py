cd93c60bb4f645772259ad52e9e7bb00a168187a
Rajan Khullar <rkhullar03@gmail.com>
2024-02-23 17:43:35 -0500

    save

--- pygo-tools/pygo_tools/backend.py
+++ pygo-tools/pygo_tools/backend.py
@@ -1,14 +1,8 @@
+from setuptools.build_meta import *
 from setuptools import build_meta as _build_meta
 from .config import Config
 from .setup import patch_wheel_darwin, precompile, build_ffi, inject_file
 import tempfile
-
-
-def get_requires_for_build_wheel(config_settings=None):
-    result = _build_meta.get_requires_for_build_wheel(config_settings)
-    if 'cffi' not in result:
-        result.append('cffi')
-    return result
 
 
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
@@ -16,9 +10,8 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
         config = Config.from_toml()
         precompile(config)
         source_ffi_path = build_ffi(config, target=temp_dir)
-        print(source_ffi_path)
         result = _build_meta.build_wheel(wheel_directory, config_settings, metadata_directory)
         inject_file(config, path=source_ffi_path)
-        # if config.platform == 'darwin':
-        #     patch_wheel_darwin(config)
+        if config.platform == 'darwin':
+            patch_wheel_darwin(config)
     return result
