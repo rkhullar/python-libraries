e567f6eeb7c7fe948ca637f88ab54d99eda0acf0
Rajan Khullar <rkhullar03@gmail.com>
2024-02-25 12:16:40 -0500

    save

--- examples/pygo-tools/_hello/docker/layer/build-external.sh
+++ examples/pygo-tools/_hello/docker/layer/build-external.sh
@@ -4,6 +4,12 @@ set -x
 here=$(dirname "$PWD"/"$0")
 here=$(cd "$here" || exit; pwd)
 
+cd "$here" || exit
 rm -rf "$here/local"; mkdir "$here/local"
 docker_context="$here/local/docker-context.tar"
 tar --exclude='local' --exclude='venv' --exclude='docker' -hcvf "$docker_context" ./*
+tar -tvf "$docker_context"
+
+docker build -t pygo-hello-build-layer - < "$docker_context"
+rm -rf "$here/local" "$here/out"
+docker compose up
