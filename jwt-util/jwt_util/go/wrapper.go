b1445705e62576ae7fb5027e80a743915ce3b5f6
Rajan Khullar <rkhullar03@gmail.com>
2024-03-03 10:56:00 -0500

    save

--- jwt-util/jwt_util/go/wrapper.go
+++ jwt-util/jwt_util/go/wrapper.go
@@ -2,6 +2,13 @@ package main
 
 import "C"
 
+//export BuildKey
+func BuildKey(size C.int, id *C.char) *C.char {
+	_id := C.GoString(id)
+	result := build_key(int(size), &_id)
+	return C.CString(result)
+}
+
 //export BuildSignature
 func BuildSignature() *C.char {
 	return C.CString(build_signature())
