c8ab5bce1c03ceb217e28226115407d0301bf3bd
Rajan Khullar <rkhullar03@gmail.com>
2024-03-19 19:42:10 -0400

    save

--- jwt-util/jwt_util/jwt_util.py
+++ jwt-util/jwt_util/jwt_util.py
@@ -8,11 +8,11 @@ KeyFormat = Literal['jwk', 'pem']
 
 def _b64_json_dumps(data: dict, sort: bool = False) -> str:
     json_data = json.dumps(data, separators=(',', ':'), sort_keys=sort)
-    return _b64_enc(json_data).rstrip('=')
+    return _b64_enc(json_data)
 
 
 def _b64_enc(data: str) -> str:
-    return base64.b64encode(data.encode()).decode()
+    return base64.b64encode(data.encode()).decode().rstrip('=')
 
 
 def encode(payload: dict, key: str, mode: KeyFormat = 'jwk', headers: dict = None) -> str:
