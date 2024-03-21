7411fcab1c6f7bab65325205f8cae12415502a40
Rajan Khullar <rkhullar03@gmail.com>
2024-03-21 18:35:20 -0400

    able to verify with jwt.io and token.dev

--- pygo-jwt/tests/test-pygo-jwt.py
+++ pygo-jwt/tests/test-pygo-jwt.py
@@ -4,3 +4,7 @@ from util import read_data, example_payload
 private_jwk = read_data('private-key.json')
 token = pygo_jwt.encode(payload=example_payload, key=private_jwk, mode='jwk', headers={'kid': 'asdf'})
 print(token)
+
+public_jwk = read_data('public-key.json')
+data = pygo_jwt.decode(token=token, key=public_jwk, mode='jwk')
+print(data)
--- pygo-jwt/tests/test-pyjwt.py
+++ pygo-jwt/tests/test-pyjwt.py
@@ -4,9 +4,9 @@ import jwt
 from util import read_data, example_payload
 
 private_pem = read_data('private-key.pem')
-public_pem = read_data('public-key.pem')
 token = jwt.encode(payload=example_payload, key=private_pem, algorithm='RS256', headers={'kid': 'asdf'})
 print(token)
 
+public_pem = read_data('public-key.pem')
 data = jwt.decode(jwt=token, key=public_pem, algorithms=['RS256'])
 print(data)
