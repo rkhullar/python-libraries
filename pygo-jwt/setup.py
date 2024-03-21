105ea7808b1200a703b85933f8e3ae3f331af6aa
Rajan Khullar <rkhullar03@gmail.com>
2024-03-21 12:10:49 -0400

    update names

--- pygo-jwt/setup.py
+++ pygo-jwt/setup.py
@@ -2,7 +2,7 @@ from pygo_tools import setup
 from setuptools import find_packages
 
 setup(
-    name='jwt-util',
+    name='pygo-jwt',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True
