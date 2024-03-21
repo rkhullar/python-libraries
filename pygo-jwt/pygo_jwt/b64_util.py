f23d598a363900b52870f32f84b9dd41c89be475
Rajan Khullar <rkhullar03@gmail.com>
2024-03-21 18:47:57 -0400

    implemented basic decode

--- pygo-jwt/pygo_jwt/b64_util.py
+++ pygo-jwt/pygo_jwt/b64_util.py
@@ -6,8 +6,15 @@ def b64_enc(data: str) -> str:
     return base64.b64encode(data.encode()).decode().rstrip('=')
 
 
+def _padding_size(data: str, n: int) -> int:
+    remainder = len(data) % n
+    return (n - remainder) % n
+
+
 def b64_dec(data: str) -> str:
-    pass
+    padding_size = _padding_size(data, n=4)
+    data += padding_size * '='
+    return base64.b64decode(data.encode()).decode()
 
 
 def b64_json_dumps(data: dict, sort: bool = False) -> str:
@@ -16,4 +23,4 @@ def b64_json_dumps(data: dict, sort: bool = False) -> str:
 
 
 def b64_json_loads(data: str):
-    pass
+    return json.loads(b64_dec(data))
