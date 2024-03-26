5b69bb2693af4809d09b5d4176555bc23559ec58
Rajan Khullar <rkhullar03@gmail.com>
2024-03-26 15:21:07 -0400

    add mutex for thread safety?

--- pygo-jwt/pygo_jwt/go/wrapper_util.go
+++ pygo-jwt/pygo_jwt/go/wrapper_util.go
@@ -2,7 +2,14 @@ package main
 
 // #include "wrapper_util.h"
 import "C"
-import "unsafe"
+import (
+	"sync"
+	"unsafe"
+)
+
+var _FreeStringMutex sync.Mutex
+var _FreeStringErrorMutex sync.Mutex
+var _FreeBoolErrorMutex sync.Mutex
 
 func TranslateStrPtr(data *C.char) *string {
 	if data != nil {
@@ -15,17 +22,32 @@ func TranslateStrPtr(data *C.char) *string {
 
 //export FreeCString
 func FreeCString(data *C.char) {
+	_FreeStringMutex.Lock()
+	defer _FreeStringMutex.Unlock()
 	C.free(unsafe.Pointer(data))
 }
 
 //export FreeStringWithError
-func FreeStringWithError(data *C.StringWithError) {
-	C.free(unsafe.Pointer(data))
+func FreeStringWithError(object *C.StringWithError) {
+	_FreeStringErrorMutex.Lock()
+	defer _FreeStringErrorMutex.Unlock()
+	if object.data != nil {
+		FreeCString(object.data)
+	}
+	if object.error != nil {
+		FreeCString(object.error)
+	}
+	C.free(unsafe.Pointer(object))
 }
 
 //export FreeBoolWithError
-func FreeBoolWithError(data *C.BoolWithError) {
-	C.free(unsafe.Pointer(data))
+func FreeBoolWithError(object *C.BoolWithError) {
+	_FreeBoolErrorMutex.Lock()
+	defer _FreeBoolErrorMutex.Unlock()
+	if object.error != nil {
+		FreeCString(object.error)
+	}
+	C.free(unsafe.Pointer(object))
 }
 
 func HandleStringWithError(res string, err error) *C.StringWithError {
@@ -43,3 +65,9 @@ func HandleBoolWithError(res bool, err error) *C.BoolWithError {
 		return &C.BoolWithError{C.bool(res), nil}
 	}
 }
+
+/*
+ * TODO
+ * - check thread safety and mutex usage; implement mutex map?
+ * - check memory leaks; use C.malloc?
+ */
