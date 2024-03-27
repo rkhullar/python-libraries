2fe0ed35209e4429ba8cb57e76867531f6e21d2d
Rajan Khullar <rkhullar03@gmail.com>
2024-03-27 16:02:45 -0400

    trying to add support for extra headers

--- pygo-jwt/pygo_jwt/go/wrapper_util.go
+++ pygo-jwt/pygo_jwt/go/wrapper_util.go
@@ -6,21 +6,8 @@ import (
 	"unsafe"
 )
 
-/*
-#ifndef WRAPPER_UTIL_H
-#define WRAPPER_UTIL_H
-
-#include <stdlib.h>
-#include <stdbool.h>
-
-typedef struct string_with_error {char* data; char* error;} StringWithError;
-typedef struct bool_with_error {bool data; char* error;} BoolWithError;
-
-#endif
-*/
+// #include "wrapper_util.h
 import "C"
-
-// TODO: change to `#include "wrapper_util.h"` once supported
 
 var _FreeStringMutex sync.Mutex
 var _FreeStringErrorMutex sync.Mutex
