5d8714afe2b7c1b1f90865382cddb65560e34fa5
Rajan Khullar <rkhullar03@gmail.com>
2024-03-31 14:43:41 -0400

    check

--- pygo-jwt/setup.py
+++ pygo-jwt/setup.py
@@ -10,7 +10,7 @@ def read_file(path: Path | str) -> str:
 
 setup(
     name='pygo-jwt',
-    version='0.0.7',
+    version='0.0.7.dev1',
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.10, <4.0',
