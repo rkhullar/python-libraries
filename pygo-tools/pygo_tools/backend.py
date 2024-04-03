227b932f0dbd86c3d57ee7d54f6a2b34035e9bdc
Rajan Khullar <rkhullar03@gmail.com>
2024-04-03 18:19:34 -0400

    save

--- pygo-tools/pygo_tools/backend.py
+++ pygo-tools/pygo_tools/backend.py
@@ -1,12 +1,13 @@
 import tempfile
+from pathlib import Path
 
 from setuptools import build_meta as _build_meta
 from setuptools.build_meta import *
 from wheel.bdist_wheel import bdist_wheel
-from pathlib import Path
 
 from .config import Config
-from .setup import build_ffi, inject_file, patch_wheel_darwin, precompile,find_wheel
+from .setup import (build_ffi, find_wheel, inject_file, patch_wheel_darwin,
+                    precompile)
 from .util import monkey_patched
 
 
@@ -19,12 +20,6 @@ class custom_bdist_wheel(bdist_wheel):
             if dep not in install_requires:
                 install_requires.append(dep)
 
-    def run(self):
-        print('hook before base run')
-        result = super(self).run()
-        print('hook after base run')
-        return result
-
 
 @monkey_patched(original=bdist_wheel, extended=custom_bdist_wheel, to_patch=['finalize_options'])
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
@@ -35,11 +30,6 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
         result = _build_meta.build_wheel(wheel_directory, config_settings, metadata_directory)
         inject_file(config, path=source_ffi_path)
         if config.platform == 'darwin':
-            print('='*100)
-            print(wheel_directory)
-            print(type(wheel_directory))
-            print()
-            print('='*100)
             wheel_path = find_wheel(config, dist_path=Path(wheel_directory))
             if not wheel_path:
                 raise FileNotFoundError(f'could not find wheel to patch')
