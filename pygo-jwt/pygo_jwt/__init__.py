c53a9d8295b7011d31124f7e03af8d3f4770d5c4
Rajan Khullar <rkhullar03@gmail.com>
2024-04-09 16:28:10 -0400

    revert

--- pygo-jwt/pygo_jwt/__init__.py
+++ pygo-jwt/pygo_jwt/__init__.py
@@ -1,4 +1,3 @@
 from .jwt_util import (decode, encode, extract_public_jwk, extract_public_pem,
                        jwk_to_pem, new_jwk, pem_to_jwk)
 from .wrapper import ExtensionAdapter
-__version__ = '0.0.9.dev1'
